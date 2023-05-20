c112204011c1100cfb45b7fe8fa17bf24b58b759
VijaySabu <130671373+VijaySabu@users.noreply.github.com>
2023-05-20 15:27:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import { StyleSheet,  View } from 'react-native';
+import ChatBot from './src';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+       <ChatBot/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+
+  },
+});
